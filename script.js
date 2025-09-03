fe10e6bcde308a53465cbd1a20ce5766fecd6909
Jonathan Lopes <jdlopes97@gmail.com>
2025-09-03 00:48:42 -0300

    create function that plays the game on console
    function plays 5 rounds, the winner is the one with the highest score

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let playerScore = 0;
 let botScore = 0;
 
-playRound(getHumanChoice(),getComputerChoice());
-
-
-
-
+playGame();
 
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 3);
@@ -34,46 +30,69 @@ function playRound(playerMove, botMove){
 
     if(playerMove == botMove){
         console.log("EMPATE");
-        return;
+        return "draw";
     }
 
     if(playerMove == "PEDRA"){
 
         if(botMove == "TESOURA"){
-            console.log("VOCÊ VENCEU");
-            return;
+            console.log("VOCÊ VENCEU, Pedra ganha de Tesoura");
+            return "player";
         }
 
         if(botMove == "PAPEL"){
-            console.log("VOCÊ PERDEU");
-            return;
+            console.log("VOCÊ PERDEU, Pedra perde para Papel");
+            return "bot";
         }
     }
 
     if(playerMove == "PAPEL"){
 
         if(botMove == "PEDRA"){
-            console.log("VOCÊ VENCEU");
-            return;
+            console.log("VOCÊ VENCEU, Papel ganha de Pedra");
+            return "player";
         }
 
         if(botMove == "TESOURA"){
-            console.log("VOCÊ PERDEU");
-            return;
+            console.log("VOCÊ PERDEU, Papel perde para Tesoura");
+            return "bot";
         }
     }
 
     if(playerMove == "TESOURA"){
 
         if(botMove == "PAPEL"){
-            console.log("VOCÊ VENCEU");
-            return;
+            console.log("VOCÊ VENCEU, Tesoura ganha de Papel");
+            return "player";
         }
 
         if(botMove == "PEDRA"){
-            console.log("VOCÊ PERDEU");
-            return;
+            console.log("VOCÊ PERDEU, Tesoura perde para Pedra");
+            return "bot";
         }
+    }
+    
+}
+
+function updateScore(result){
+    if(result == "player") playerScore++;
+    if(result == "bot") botScore++;
+    console.log("\tPontuação:\n Player: " + playerScore + "\tComputador: " + botScore +"\n");
+
+}
+
+function playGame(){
+    let roundWinner = "";
+    for(let i = 0; i < 5; i++){
+        roundWinner = playRound(getHumanChoice(),getComputerChoice());
+        updateScore(roundWinner);
+    }
+    if(playerScore > botScore){
+        console.log("Você venceu o jogo, Parabéns!!!!")
+    }else if(playerScore < botScore){
+        console.log("Você perdeu o jogo, que pena!")
+    }else{
+        console.log("O jogo terminou em empate");
     }
     
 }
