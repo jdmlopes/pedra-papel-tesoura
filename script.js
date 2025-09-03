717fdb7351a3e40a7f46b7f7ddd17e3b952853b7
Jonathan Lopes <jdlopes97@gmail.com>
2025-09-02 23:56:52 -0300

    create function for npc move
    created function that returns the move the npc plays by generating a random number

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Pode Começar a desenvolver");
+let botMove = getComputerChoice()
+
+console.log("O computador escolheu " + botMove);
+
+
+
+function getComputerChoice(){
+    let rand = Math.floor(Math.random() * 3);
+    switch(rand){
+        case 0:
+            return "Pedra";
+            break;
+        case 1:
+            return "Papel";
+            break;
+        case 2:
+            return "Tesoura";
+            break;
+        default:
+            console.log("Algo inexperado aconteceu");
+    }
+}
